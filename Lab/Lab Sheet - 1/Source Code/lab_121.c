f81aacefb7ca01e14d654b5b56d2977a0c4995f0
Santa <santa@c2.my>
2022-07-13 17:02:15 +0545

    Lab source for sheet 1, 1a added.

---
+++ Lab/Lab Sheet - 1/Source Code/lab_121.c
@@ -0,0 +1,16 @@
+/**
+ * Hello World Program in C.
+ *
+ * By: Santa Basnet
+ * Everest Engineering College.
+ * Date: 2022-07-10
+ */
+/** A Header File **/
+#include <stdio.h>
+/** Main Program Starts Here **/
+int main() {
+    /** Display welcome message to the Console. **/
+    printf("Welcome to the programming world !\n");
+    /** The main function wants to return an integer value. **/
+    return(0);
+}
---
+++ Lab/Lab Sheet - 1/Source Code/lab_1a.c
@@ -0,0 +1,20 @@
+/**
+* Drawing shapes using printf() function.
+*
+* By: Santa Basnet
+* Everest Engineering College.
+* Date: 2022-07-10
+*/
+/** A Header File **/
+#include <stdio.h>
+/** Main Program Starts Here, that prints PI. **/
+int main() {
+    printf("\n=============");
+    printf("\n   *     **  ");
+    printf("\n   *     **  ");
+    printf("\n   *     **  ");
+    printf("\n   *     **  ");
+    printf("\n* *      **  ");
+    printf("\n *       **  ");
+    return(0);
+}
